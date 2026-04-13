6db7136a705f13dfd94c2c159451eff9411a3de0
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2026-04-13 13:05:25 -0400

    wip basket drops

--- common/src/main/java/com/lucyazalea/cobblemonwildloot/mixin/PokemonEntityTickMixin.java
+++ common/src/main/java/com/lucyazalea/cobblemonwildloot/mixin/PokemonEntityTickMixin.java
@@ -1,7 +1,6 @@
 package com.lucyazalea.cobblemonwildloot.mixin;
 
 import com.lucyazalea.cobblemonwildloot.CobblemonWildLoot;
-import com.lucyazalea.cobblemonwildloot.CobblemonWildLootConfig;
 import com.cobblemon.mod.common.api.drop.DropEntry;
 import com.cobblemon.mod.common.api.drop.DropTable;
 import com.cobblemon.mod.common.api.drop.ItemDropEntry;
@@ -9,15 +8,10 @@ import com.cobblemon.mod.common.entity.pokemon.PokemonEntity;
 import com.cobblemon.mod.common.pokemon.FormData;
 import com.lucyazalea.cobblemonwildloot.PokebasketEntity;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.item.ItemEntity;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -36,8 +30,7 @@ public class PokemonEntityTickMixin {
         var world = entity.level();
         if (world == null || world.isClientSide) return;
 
-        // 10% drop rate per minute with default config.
-        double dropChance = CobblemonWildLoot.CONFIG.getDropChance() / CobblemonWildLoot.CONFIG.getDropCheckTicks();
+        double dropChance = 12 * CobblemonWildLoot.CONFIG.getDropChance() / CobblemonWildLoot.CONFIG.getDropCheckTicks();
         double randomNumber = Math.random();
         if (randomNumber < dropChance) {
             var pokemon = entity.getPokemon();
@@ -52,10 +45,20 @@ public class PokemonEntityTickMixin {
 
                     DropEntry drop = drops.get(world.random.nextInt(drops.size()));
                     if (drop instanceof ItemDropEntry itemDropEntry && !Arrays.asList(CobblemonWildLoot.CONFIG.getItemBlacklist()).contains(itemDropEntry.getItem().toString())) {
-                        itemDropEntry.drop((LivingEntity) entity, (ServerLevel) world, entity.position(), null);
+                        var droppedIntoBasket = false;
+                        BlockPos centerPos = entity.blockPosition();
+                        BlockPos.betweenClosed(centerPos.offset(-10, -10, -10), centerPos.offset(10, 10, 10)).forEach(pos -> {
+                            BlockEntity blockEntity = world.getBlockEntity(pos);
+                            if (blockEntity instanceof PokebasketEntity) {
+                                out.println("Pokebasket!");
+                            }
+                        });
+                        if (!droppedIntoBasket) {
+                            itemDropEntry.drop((LivingEntity) entity, (ServerLevel) world, entity.position(), null);
+                        }
                     }
                 } catch (Exception e) {
-                    CobblemonWildLoot.LOGGER.error(e.toString());
+                    out.println("Error: " + e.toString());
                 }
             }
         }
