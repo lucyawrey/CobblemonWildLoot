1b7c4a7d6164463f5315fb83863b507e14eb0c0c
Lucy Awrey <35198881+lucyawrey@users.noreply.github.com>
2026-04-22 11:04:33 -0400

    handle ground drops directly, handle item drop percentage chances, roll entire pokemon loot table each time a pokemon drops items

--- common/src/main/java/com/lucyazalea/cobblemonwildloot/mixin/PokemonEntityTickMixin.java
+++ common/src/main/java/com/lucyazalea/cobblemonwildloot/mixin/PokemonEntityTickMixin.java
@@ -12,7 +12,7 @@ import kotlin.ranges.RangesKt;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -35,8 +35,7 @@ public class PokemonEntityTickMixin {
         if (world == null || world.isClientSide) return;
 
         double dropChance = CobblemonWildLoot.CONFIG.getDropChance() / CobblemonWildLoot.CONFIG.getDropCheckTicks();
-        double randomNumber = Math.random();
-        if (randomNumber < dropChance) {
+        if (Math.random() < dropChance) {
             var pokemon = entity.getPokemon();
             if (pokemon == null) return;
 
@@ -45,29 +44,32 @@ public class PokemonEntityTickMixin {
                     FormData form = pokemon.getForm();
                     DropTable dropTable = form.getDrops();
                     List<DropEntry> drops = dropTable.getDrops(dropTable.getAmount(), pokemon);
-
                     if (drops.isEmpty()) return;
-                    DropEntry drop = drops.get(world.random.nextInt(drops.size()));
 
-                    if (drop instanceof ItemDropEntry itemDropEntry && !CobblemonWildLoot.CONFIG.getItemBlacklist().contains(itemDropEntry.getItem().toString())) {
-                        var droppedIntoBasket = false;
-                        BlockPos centerPos = entity.blockPosition();
-                        var rad = CobblemonWildLoot.CONFIG.getPokebasketBlockRadius();
-                        for (var pos : BlockPos.betweenClosed(centerPos.offset(-rad, -rad, -rad), centerPos.offset(rad, rad, rad))) {
-                            BlockEntity blockEntity = world.getBlockEntity(pos);
-                            if (blockEntity instanceof PokebasketEntity basket) {
-                                var item = world.registryAccess().registryOrThrow(Registries.ITEM).get(itemDropEntry.getItem());
-                                if (item != null) {
-                                    var quantity = itemDropEntry.getQuantityRange() != null ? RangesKt.random(itemDropEntry.getQuantityRange(), Random.Default) : itemDropEntry.getQuantity();
-                                    var stack = new ItemStack(item, quantity);
-                                    if (basket.addStack(stack)) {
-                                        droppedIntoBasket = true;
+                    for (var drop : drops) {
+                        if (drop instanceof ItemDropEntry itemDropEntry && !CobblemonWildLoot.CONFIG.getItemBlacklist().contains(itemDropEntry.getItem().toString())) {
+                            var item = world.registryAccess().registryOrThrow(Registries.ITEM).get(itemDropEntry.getItem());
+                            var quantity = itemDropEntry.getQuantityRange() != null ? RangesKt.random(itemDropEntry.getQuantityRange(), Random.Default) : itemDropEntry.getQuantity();
+                            var chance = itemDropEntry.getPercentage() == 100f || Math.random() * 100f < itemDropEntry.getPercentage();
+                            if (item != null && quantity > 0 && chance) {
+                                var stack = new ItemStack(item, quantity);
+
+                                var droppedIntoBasket = false;
+                                BlockPos centerPos = entity.blockPosition();
+                                var rad = CobblemonWildLoot.CONFIG.getPokebasketBlockRadius();
+                                for (var pos : BlockPos.betweenClosed(centerPos.offset(-rad, -rad, -rad), centerPos.offset(rad, rad, rad))) {
+                                    BlockEntity blockEntity = world.getBlockEntity(pos);
+                                    if (blockEntity instanceof PokebasketEntity basket) {
+                                        if (basket.addStack(stack)) {
+                                            droppedIntoBasket = true;
+                                        }
                                     }
                                 }
+                                if (!droppedIntoBasket) {
+                                    var pos = entity.position();
+                                    world.addFreshEntity(new ItemEntity(world, pos.x, pos.y, pos.z, stack));
+                                }
                             }
-                        }
-                        if (!droppedIntoBasket) {
-                            itemDropEntry.drop((LivingEntity) entity, (ServerLevel) world, entity.position(), null);
                         }
                     }
                 } catch (Exception e) {
